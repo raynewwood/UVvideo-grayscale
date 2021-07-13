4470796b535cfea6ea83499ae0ee7a80c37bbf67
raynewwood <67037182+raynewwood@users.noreply.github.com>
2021-07-13 22:19:13 +0800

    Add files via upload

--- save.py
+++ save.py
@@ -1,10 +1,16 @@
+#version 1.1.1
+#change time 2021 07 13 21:04
+#developed by Leilei Niu
 import numpy as np
 import cv2
 import math
+import datetime
 
 
-input='1s'
+input='2s'
 nihe =1
+thresh_v=240#二值化阈值
+
 #1 圆拟合
 #2 直边界矩形
 #3 最小矩形
@@ -14,27 +20,45 @@ cap = cv2.VideoCapture('%s.mp4'%input)
 # Define the codec and create VideoWriter object
 size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
         int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-out = cv2.VideoWriter('output%s.mp4'%input, cv2.VideoWriter_fourcc(*'XVID'), 20.0, size,1)
-threshold=size[0]*size[1]/1500
+out = cv2.VideoWriter('C_T_%s-N_%d-Th_%d.mp4'%(input,nihe,thresh_v), cv2.VideoWriter_fourcc(*'XVID'), 25.0, size,1)#C_T_%s-N_%d-Th_%d.jpg'%(input,nihe,thresh_v)
+threshold=size[0]*size[1]/1000#小面积消除阈值
 f=0
 count=1
 size_w=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 size_h=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 data_A = np.zeros([size_w, size_h], np.uint8)
+k=0
+starttime = datetime.datetime.now()
+
 while(cap.isOpened()):
+
+    file__ = open('C_T_%s_N_%d_Thb_%d_Thl%d.txt' % (input, nihe, thresh_v, threshold), 'a')
+
+    #file__ = open('C.txt', 'a')
     ret, frame = cap.read()
+
     if nihe ==1: #圆拟合
+
         if ret==True:
+
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#灰度化
-            ret, thresh = cv2.threshold(gray, 240, 255, 0)  # 二值化
+            ret, thresh = cv2.threshold(gray, thresh_v, 255, 0)  # 二值化阈值
             kernel = np.ones((5, 5), np.uint8)
             opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)#开运算
             closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)#闭运算
             contours, hierarchy = cv2.findContours(closing, 3, 1)#轮廓检测
+            frame_area=0
+            frame_are_nihe=0
+            k = k + 1
             for c in range(len(contours)):
-                if cv2.contourArea(contours[c])< threshold:
+                area_contour =cv2.contourArea(contours[c])
+
+                if area_contour< threshold:
                     continue
+
                 (x, y), radius = cv2.minEnclosingCircle(contours[c])
+                frame_area += area_contour
+                frame_are_nihe +=np.pi*radius*radius
                 center = (int(x), int(y))
                 radius = int(radius)
                 area = cv2.contourArea(contours[c])
@@ -48,34 +72,45 @@ while(cap.isOpened()):
                             data_A[i][j]+=1#若在圆内，则将该像素点置1
                 cv2.putText(frame, text1, (10, 30), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA, 0)
                 cv2.putText(frame, text2, (10, 60), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA, 0)
+
                 out.write(frame)
                 cv2.imshow('frame',frame)
                 # 若没有按下q键，则每1毫秒显示一帧
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
+            file__.write(str(k))
+            file__.write(' ')
+            file__.write(str(frame_area))
+            file__.write(' ')
+            file__.write(str(frame_are_nihe))
+            file__.write('\n')
 
         else:
             break
 
-
-
-
     elif nihe ==2:#2 直边界矩形
+
         if ret==True:
             #frame = cv2.flip(frame,0)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#灰度化
-            ret, thresh = cv2.threshold(gray, 230, 255, 0)  # 二值化
+            ret, thresh = cv2.threshold(gray, thresh_v, 255, 0)  # 二值化
             kernel = np.ones((5, 5), np.uint8)
             opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)#开运算
             closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)#闭运算
             contours, hierarchy = cv2.findContours(closing, 3, 1)#轮廓检测
+            frame_area=0
+            frame_are_nihe=0
+            k = k + 1
             for c in range(len(contours)):
-                if cv2.contourArea(contours[c])< threshold:
+                area_contour = cv2.contourArea(contours[c])
+                if area_contour < threshold:
                     continue
+                frame_area += area_contour
                 x, y, w, h = cv2.boundingRect(contours[c])
                 area = cv2.contourArea(contours[c])
                 aspect_ratio = float(w) / h  # 长宽比
                 rect_area = w * h
+                frame_are_nihe+=rect_area
                 extent = float(area) / rect_area  # 轮廓面积与边界矩形面积的比
                 hull = cv2.convexHull(contours[c])
                 hull_area = cv2.contourArea(hull)
@@ -97,21 +132,36 @@ while(cap.isOpened()):
                 cv2.imshow('frame',frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+            file__.write(str(k))
+            file__.write(' ')
+            file__.write(str(frame_area))
+            file__.write(' ')
+            file__.write(str(frame_are_nihe))
+            file__.write('\n')
+
         else:
             break
 
+
+
     elif nihe ==3:#3 最小矩形
+
         if ret==True:
             #frame = cv2.flip(frame,0)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#灰度化
-            ret, thresh = cv2.threshold(gray, 230, 255, 0)  # 二值化
+            ret, thresh = cv2.threshold(gray, thresh_v, 255, 0)  # 二值化
             kernel = np.ones((5, 5), np.uint8)
             opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)#开运算
             closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)#闭运算
             contours, hierarchy = cv2.findContours(closing, 3, 1)#轮廓检测
+            frame_area=0
+            frame_are_nihe=0
+            k=k+1
             for c in range(len(contours)):
-                if cv2.contourArea(contours[c])< threshold:
+                area_contour = cv2.contourArea(contours[c])
+                if area_contour < threshold:
                     continue
+                frame_area += area_contour
                 rect = cv2.minAreaRect(contours[c])
                 box = cv2.boxPoints(rect)
                 box = np.int0(box)  # 获得矩形角点
@@ -134,6 +184,7 @@ while(cap.isOpened()):
                 maxy=int(-t_x*sin+t_y*cos)
 
                 area = cv2.contourArea(box)
+                frame_are_nihe+=area
                 width = rect[1][0]
                 height = rect[1][1]
                 cv2.polylines(frame, [box], True, (0, 255, 0), 3)#原画中画轮廓
@@ -155,22 +206,38 @@ while(cap.isOpened()):
                 cv2.imshow('frame',frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+            file__.write(str(k))
+            file__.write(' ')
+            file__.write(str(frame_area))
+            file__.write(' ')
+            file__.write(str(frame_are_nihe))
+            file__.write('\n')
+
         else:
             break
 
+
     elif nihe ==4:#4 椭圆
+        
+
         if ret==True:
             #frame = cv2.flip(frame,0)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#灰度化
-            ret, thresh = cv2.threshold(gray, 240, 255, 0)  # 二值化
+            ret, thresh = cv2.threshold(gray, thresh_v, 255, 0)  # 二值化
             kernel = np.ones((5, 5), np.uint8)
             opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)#开运算
             closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)#闭运算
             contours, hierarchy = cv2.findContours(closing, 3, 1)#轮廓检测
+            frame_area=0
+            frame_are_nihe=0
 
+            k=k+1
             for c in range(len(contours)):
-                if cv2.contourArea(contours[c])< threshold:
+                area_contour = cv2.contourArea(contours[c])
+                if area_contour < threshold:
                     continue
+                frame_area += area_contour
+
                 (cx,cy),(a,b),angle = cv2.fitEllipse(contours[c])#（x, y）代表椭圆中心点的位置
                                                                  #（a, b）代表长短轴长度，应注意a、b为长短轴的直径，而非半径
                                                                  # #angle 代表了中心旋转的角度
@@ -185,6 +252,7 @@ while(cap.isOpened()):
                 #print(cx,cy,a,b,angle)
                 cv2.ellipse(frame,(np.int32(cx),np.int32(cy)),
                             (np.int32(a/2), np.int32(b/2)), angle, 0, 360, (0, 0, 255), 2, 8, 0)
+                frame_are_nihe+=np.pi*a*b/4
                 for i in range(0,size[0]):
                     for j in range(0,size[1]):
                         #print(int(i*cos)+int(j*sin),cx,int(-i*sin)+int(j*cos),cy,a,b)
@@ -200,20 +268,33 @@ while(cap.isOpened()):
                 cv2.imshow('frame',frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+            file__.write(str(k))
+            file__.write(' ')
+            file__.write(str(frame_area))
+            file__.write(' ')
+            file__.write(str(frame_are_nihe))
+            file__.write('\n')
+
+
         else:
             break
-    else:
-        break
 
+    else:
+        print("请输入合法数据（1-4）")
+        break
+    file__.close()
+endtime = datetime.datetime.now()
+print((endtime - starttime).seconds)
+
+data_A=np.transpose(data_A)
 cv2.imshow('grayimage', data_A)
 cv2.imwrite('%s.jpg'%input, data_A)
 M=np.max(data_A)#均一化
-print(M)
 data_B=data_A/M
 data_C=data_B*255
 b = data_C.astype(np.uint8)
 color=cv2.applyColorMap(b, cv2.COLORMAP_JET)
-cv2.imwrite('%s-%d.jpg'%(input,nihe), color)
+cv2.imwrite('C_T_%s-N_%d-Thb_%d-M%d-Thl%d.jpg'%(input,nihe,thresh_v,M,threshold), color)#C T N Thb m Thl分别表示彩色，视频时长，拟合类型，灰度化阈值，矩阵最大值，小面积消除阈值
 cap.release()
 out.release()
 cv2.destroyAllWindows()
